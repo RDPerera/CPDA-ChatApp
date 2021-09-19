e9a7547e0f0e50d8660d365d05d7dfc5c75258af
Dilan Perera <39415471+RDPerera@users.noreply.github.com>
2021-09-19 22:21:21 +0530

    React App

--- cpda-chat/src/App.js
+++ cpda-chat/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./sliitlogo.png";
+import chatlogo from "./pngegg.png";
+import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    addResponseMessage("Welcome to **CPDA Bot** test!");
+  }, []);
+
+  const handleNewUserMessage = async (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    //send message to the server
+    const response = await axios.post("http://127.0.0.1:5000/hello", {
+      message: "hi",
+    });
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            <span className="main-title">CPDA Chat</span>
+          </Navbar.Brand>
+          <Nav className="me-auto"></Nav>
+        </Container>
+      </Navbar>
+      <div className="wrapper">
+        <div className="header">
+          <h2>Welcome to the CPDA Bot Test Application</h2>
+        </div>
+        <div className="intro">
+          <p>
+            This is a testing application for classification model of the{" "}
+            <b>Cancer Patient Distress Assistant</b>.
+          </p>
+        </div>
+        <div className="content">
+          <Widget
+            handleNewUserMessage={handleNewUserMessage}
+            profileAvatar={chatlogo}
+            title="CPDA Chat"
+            subtitle="Cancer Patient Distress Assistant Chat Application"
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
